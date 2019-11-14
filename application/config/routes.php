a1873b90f69fcdc3246c890dd873dcba55b31958
Nguyen Tan Tai <tainguyen.uit@gmail.com>
2019-11-14 23:03:58 +0700

    Change route login

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'home/gotohome';
 $route['admin'] = 'admin/home';
-$route['admin/login/login']['post'] = 'admin/login/login';
+$route['admin/login']['post'] = 'admin/login/login';
 $route['admin/login']['get'] = 'admin/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
