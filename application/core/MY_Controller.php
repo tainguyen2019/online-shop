452710f070e2b6211454469c9f8f2a99bc22ae59
Nguyen Tan Tai <tainguyen.uit@gmail.com>
2019-11-14 22:43:30 +0700

    Fix redirect overtime

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,16 +3,13 @@ class MY_Controller extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
-        if ($this->session->userdata('isLogged')) {
-            redirect('admin');
-        } else {
+        if (!$this->session->userdata('isLogged')) {
             redirect('admin/login');
         }
+
     }
+
+
 }
-
-
-
-
 
 ?>
