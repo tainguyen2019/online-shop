239c1669002a916eb344c4b5d61001f0a1982a68
tainguyen2019 <54633651+tainguyen2019@users.noreply.github.com>
2019-11-17 22:32:02 +0700

    add constructor

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -1,6 +1,11 @@
 <?php
 class Login extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
     public function index(){
         $this->load->view('admin/LoginPage');
     }
